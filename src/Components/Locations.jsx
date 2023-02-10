a17d0cd81ff351fd2915eb60b31be93830f75721
DianaBerte <dianaberte.go@gmail.com>
2023-02-10 15:44:30 +0100

    Update Locations.jsx

--- src/Components/Locations.jsx
+++ src/Components/Locations.jsx
@@ -2,15 +2,25 @@
 // import { Badge } from 'react-bootstrap'
 // import { Link } from 'react-router-dom'
 import ListGroup from 'react-bootstrap/ListGroup'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Locations = () => {
+
+    const navigate = useNavigate()
+
     return (
         <>
     <ListGroup variant="flush">
-      <ListGroup.Item>Cras justo odio</ListGroup.Item>
-      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item>Cras justo odio <br />
+      <Button variant="primary" onClick={() => navigate('/about')} >About</Button>
+      </ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in <br />
+      <Button variant="primary" onClick={() => navigate('/about')} >About</Button></ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus <br />
+      <Button variant="primary" onClick={() => navigate('/about')} >About</Button></ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac <br />
+      <Button variant="primary" onClick={() => navigate('/about')} >About</Button></ListGroup.Item>
     </ListGroup>
       </>
     )
